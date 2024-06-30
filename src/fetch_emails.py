b9fa061959c9e79788b78d53f4c2e4e9d4defdeb
aditya6981 <41117410+aditya6981@users.noreply.github.com>
2024-06-30 21:00:18 +0530

    bugfix

--- src/fetch_emails.py
+++ src/fetch_emails.py
@@ -14,36 +14,39 @@ def fetch_emails(gmail_service, session):
     results = gmail_service.users().messages().list(userId="", maxResults=10).execute()  # Fetch the list of emails
     messages = results.get("messages", [])
     email_list = []
-    for message in messages:
-        msg = gmail_service.users().messages().get(userId="me", id=message["id"]).execute()
-        payload = msg["payload"]
-        headers = payload["headers"]
-        labels = msg["labelIds"]
+    try:
+        for message in messages:
+            msg = gmail_service.users().messages().get(userId="me", id=message["id"]).execute()
+            payload = msg["payload"]
+            headers = payload["headers"]
+            labels = msg["labelIds"]
 
-        if "data" in msg["payload"]["body"]:
-            body = base64.urlsafe_b64decode(msg["payload"]["body"]["data"]).decode("utf-8")
-        else:
-            for part in msg["payload"]["parts"]:
-                if "data" in part["body"]:
-                    body = base64.urlsafe_b64decode(part["body"]["data"]).decode("utf-8")
+            if "data" in msg["payload"]["body"]:
+                body = base64.urlsafe_b64decode(msg["payload"]["body"]["data"]).decode("utf-8")
+            else:
+                for part in msg["payload"]["parts"]:
+                    if "data" in part["body"]:
+                        body = base64.urlsafe_b64decode(part["body"]["data"]).decode("utf-8")
 
-        email_data = {
-            "message_id": message["id"],
-            "sender": next(header["value"] for header in headers if header["name"] == "From"),
-            "recipient": next(header["value"] for header in headers if header["name"] == "To"),
-            "subject": next(header["value"] for header in headers if header["name"] == "Subject"),
-            "body": body,
-            "received_date": datetime.datetime.fromtimestamp(int(msg["internalDate"]) / 1000),
-            "read_status": labels[0],
-            "mailbox": labels[2]
-        }
+            email_data = {
+                "message_id": message["id"],
+                "sender": next(header["value"] for header in headers if header["name"] == "From"),
+                "recipient": next(header["value"] for header in headers if header["name"] == "To"),
+                "subject": next(header["value"] for header in headers if header["name"] == "Subject"),
+                "body": body,
+                "received_date": datetime.datetime.fromtimestamp(int(msg["internalDate"]) / 1000),
+                "read_status": labels[0],
+                "mailbox": labels[2]
+            }
 
-        email_list.append(email_data)
+            email_list.append(email_data)
+        save_emails(session, email_list, Email)
+        session.commit()
+
+    except Exception:
+        email_list = []
 
     print("Total Emails Fetched : ", len(email_list))
-
-    save_emails(session, email_list, Email)
-    session.commit()
 
 
 def main():
